1bd26e4628305ba8cce7085566fd48616e6bb887
Yanni Fraga <yanni.ribeiro.fraga@avanade.com>
2019-02-02 15:50:12 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Jokes
 
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
 
 ## Development server
@@ -25,3 +26,7 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+
+[https://www.youtube.com/watch?v=othhfZ0mGjU](https://www.youtube.com/watch?v=othhfZ0mGjU)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jokes';
+  joke: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => document.location.reload()); // if there an update, the page will reload
+    });
+  }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res
+      this.joke = this.joke.replace(/&quot;/g,'"');
+    });
+  }
 }
+
+/* 
+  Things to add:
+  - push notification
+  - add icon to home screen
+*/
